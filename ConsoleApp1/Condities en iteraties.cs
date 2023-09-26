aef7551beca705c20c803661ff539f1dfab156cd
lolpowerluke <47672899+lolpowerluke@users.noreply.github.com>
2023-09-26 14:20:46 +0200

    Added 2 exercises

---
+++ ConsoleApp1/Condities en iteraties.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Condities_en_iteraties
+    {
+        public static void main()
+        {
+            Console.WriteLine("give word!");
+            string word = Console.ReadLine();
+            char[] charArray1 = word.ToCharArray();
+            Array.Reverse(charArray1);
+
+            Console.WriteLine("For");
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                Console.Write(charArray1[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Do while");
+            int j = 0;
+            do
+            {
+                Console.Write(charArray1[j]);
+                j++;
+
+            } while (j < charArray1.Length);
+
+            Console.WriteLine();
+            Console.WriteLine("While");
+            int k = 0;
+            while (k < charArray1.Length)
+            {
+                Console.Write(charArray1[k]);
+                k++;
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Recursion");
+            int m = 0;
+            recursion();
+            void recursion()
+            {
+                Console.Write(charArray1[m]);
+                m++;
+                if (m < charArray1.Length)
+                {
+                    recursion();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Werken met arrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Werken_met_arrays
+    {
+        public static string[] createArray()
+        {
+            bool isValid = false;
+            int lenght;
+            do
+            {
+                Console.WriteLine("How long should the array be?");
+                string input = Console.ReadLine();
+                isValid = int.TryParse(input, out lenght);
+
+            } while(isValid == false);
+            string[] array = new string[lenght];
+            for(int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine("Give element " + (i+1));
+                array[i] = Console.ReadLine();
+            }
+            return array;
+        }
+    }
+}
